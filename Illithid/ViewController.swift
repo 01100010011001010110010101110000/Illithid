d64ed48358d7667fc79fd70a96244474dcc67ab1
Tyler Gregory <td.g@icloud.com>
2018-11-17 23:31:02 -0600

    Initial Commit

---
+++ Illithid/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Illithid
+//
+//  Created by Tyler Gregory on 11/17/18.
+//  Copyright © 2018 flayware. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
