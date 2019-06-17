c06586a50b9b83a9c11d56bfe617f9899e12d5f4
Tyler Gregory <td.g@icloud.com>
2019-06-17 17:16:36 -0500

    Handle removal of NavigationView support for macOS

---
+++ Sources/Illithid/RedditClient/Extensions/Array.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//
+//
+//  Created by Tyler Gregory on 6/17/19.
+//
+
+import Foundation
+
+public extension Array {
+  func middle() -> Element {
+    let middleIndex = self.isEmpty ? 0 : self.count / 2
+    return self[middleIndex]
+  }
+}
