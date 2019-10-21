4970f70f30ecf06394a97f073d3999fc05d2235d
Tyler Gregory <tdgregory@protonmail.com>
2019-10-20 19:53:30 -0500

    Bump `KeychainAccess` to new minor release

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
       .package(url: "https://github.com/01100010011001010110010101110000/OAuthSwift.git", from: .init(2, 0, 1)),
       .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: .init(5, 0, 0)),
       .package(url: "https://github.com/Nike-Inc/Willow.git", from: .init(6, 0, 0)),
-      .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", from: .init(3, 2, 0))
+      .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", from: .init(3, 2, 1))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
