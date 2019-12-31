4d82117cb2b024c0d328863370ee3be231e47bfa
Tyler Gregory <tdgregory@protonmail.com>
2019-12-30 23:01:42 -0600

    Introduce RedditURL type for wrapping URL fields returned as the empty string

---
+++ Sources/Illithid/Utility/RedditURL.swift
@@ -0,0 +1,30 @@
+//
+// {file}
+// Copyright (c) 2019 Flayware
+// Created by Tyler Gregory (@01100010011001010110010101110000) on {created}
+//
+
+import Foundation
+
+/// This is a wrapper struct to make decoding URL types from Reddit's API more convenient
+/// Reddit returns the empty string when an object does not have the content it would otherwise link to via a URL
+public struct RedditURL: Codable {
+  public let url: URL?
+
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.singleValueContainer()
+    if container.decodeNil() {
+      url = nil
+    }
+    else if let emptyString = try? container.decode(String.self), emptyString.isEmpty {
+      url = nil
+    } else {
+      url = try container.decode(URL.self)
+    }
+  }
+
+  public func encode(to encoder: Encoder) throws {
+    var container = encoder.singleValueContainer()
+    try container.encode(url)
+  }
+}
