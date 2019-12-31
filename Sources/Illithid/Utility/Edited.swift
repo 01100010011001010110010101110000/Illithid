bad20676b1c058cdf8c13037d821d85f8ec564e4
Tyler Gregory <tdgregory@protonmail.com>
2019-12-30 23:01:11 -0600

    Properly handle nil date values in the edited struct Encode the value in a way that is compatible with decodable

---
+++ Sources/Illithid/Utility/Edited.swift
@@ -0,0 +1,30 @@
+//
+// Edited.swift
+// Copyright (c) 2019 Flayware
+// Created by Tyler Gregory (@01100010011001010110010101110000) on 12/24/19
+//
+
+import Foundation
+
+/// The Reddit API returns either `false` or a UTC timestamp for this field, so if we can decode a Bool it has not been edited,
+/// otherwise we should deocde the edited date
+public struct Edited: Codable {
+  public let on: Date?
+
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.singleValueContainer()
+    if container.decodeNil() {
+      on = nil
+    }
+    else if let _ = try? container.decode(Bool.self) {
+      on = nil
+    } else {
+      on = try container.decode(Date.self)
+    }
+  }
+
+  public func encode(to encoder: Encoder) throws {
+    var container = encoder.singleValueContainer()
+    try container.encode(on)
+  }
+}
