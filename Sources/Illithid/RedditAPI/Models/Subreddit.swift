0d4e7256bdafe89e2df1a29b5aac482cb413be55
Tyler Gregory <tdgregory@protonmail.com>
2020-02-16 13:45:41 -0600

    Add initial sidebar support

--- Sources/Illithid/RedditAPI/Models/Subreddit.swift
+++ Sources/Illithid/RedditAPI/Models/Subreddit.swift
@@ -25,11 +25,14 @@ public struct Subreddit: RedditObject {
   public let id: String // swiftlint:disable:this identifier_name
   public let name: String
   public let publicDescription: String
+  public let publicDescriptionHtml: String?
   public let displayName: String
   public let wikiEnabled: Bool?
   public let headerImg: URL?
   public let over18: Bool?
   public let createdUtc: Date
+  public let description: String
+  public let descriptionHtml: String?
 
   /// The Reddit API sometimes returns the empty string for the `header_img` parameter, and also may return `nil`, so we handle the empty srtring, then
   /// decode the actual URL if it is present
@@ -39,10 +42,13 @@ public struct Subreddit: RedditObject {
     id = try container.decode(String.self, forKey: .id)
     name = try container.decode(String.self, forKey: .name)
     publicDescription = try container.decode(String.self, forKey: .publicDescription)
+    publicDescriptionHtml = try container.decodeIfPresent(String.self, forKey: .publicDescriptionHtml)
     displayName = try container.decode(String.self, forKey: .displayName)
     wikiEnabled = try container.decodeIfPresent(Bool.self, forKey: .wikiEnabled)
     over18 = try container.decodeIfPresent(Bool.self, forKey: .over18)
     createdUtc = try container.decode(Date.self, forKey: .createdUtc)
+    description = try container.decode(String.self, forKey: .description)
+    descriptionHtml = try container.decodeIfPresent(String.self, forKey: .descriptionHtml)
 
     if let emptyString = try? container.decodeIfPresent(String.self, forKey: .headerImg), emptyString.isEmpty {
       headerImg = nil
@@ -55,10 +61,13 @@ public struct Subreddit: RedditObject {
     case id // swiftlint:disable:this identifier_name
     case name
     case publicDescription
+    case publicDescriptionHtml
     case displayName
     case wikiEnabled
     case headerImg
     case over18
     case createdUtc
+    case description
+    case descriptionHtml
   }
 }
