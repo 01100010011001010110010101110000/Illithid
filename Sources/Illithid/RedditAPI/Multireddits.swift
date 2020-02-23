4c7310ead201ba8707de742b6b5679f0a21596ec
Tyler Gregory <tdgregory@protonmail.com>
2020-02-23 12:23:44 -0600

    Xcode missed these

--- Sources/Illithid/RedditAPI/Multireddits.swift
+++ Sources/Illithid/RedditAPI/Multireddits.swift
@@ -18,3 +18,36 @@ extension Multireddit: PostsProvider {
     }
   }
 }
+
+public extension Illithid {
+  func addSubreddit(to multireddit: Multireddit, subreddit: Subreddit, completion: @escaping (Result<Data?, AFError>) -> Void) {
+    let updateUrl = URL(string: "/api/multi\(multireddit.path.absoluteString)r/\(subreddit.displayName)", relativeTo: baseURL)!
+
+    // The reddit API requires that `model` be JSON, but that the whole payload be URL encoded
+    let updateModel = [
+      "model": "{\"name\": \"\(subreddit.displayName)\"}"
+    ]
+
+    session.request(updateUrl, method: .put, parameters: updateModel, encoding: URLEncoding(destination: .httpBody)).validate().response { response in
+      completion(response.result)
+    }
+  }
+
+  func removeSubreddit(from multireddit: Multireddit, subreddit: Multireddit.MultiSubreddit, completion: @escaping (Result<Data?, AFError>) -> Void) {
+    let deleteUrl = URL(string: "/api/multi/\(multireddit.path.absoluteString)/r/\(subreddit.name)", relativeTo: baseURL)!
+
+    session.request(deleteUrl, method: .delete).validate().response { response in
+      completion(response.result)
+    }
+  }
+}
+
+public extension Multireddit {
+  func removeSubreddit(_ subreddit: MultiSubreddit, completion: @escaping (Result<Data?, AFError>) -> Void) {
+    Illithid.shared.removeSubreddit(from: self, subreddit: subreddit, completion: completion)
+  }
+
+  func addSubreddit(_ subreddit: Subreddit, completion: @escaping (Result<Data?, AFError>) -> Void) {
+    Illithid.shared.addSubreddit(to: self, subreddit: subreddit, completion: completion)
+  }
+}
