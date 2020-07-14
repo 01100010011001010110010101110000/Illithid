6b6b8eeb6f2386d833420dcf415816ecfe7c197f
Tyler Gregory <tdgregory@protonmail.com>
2020-07-13 22:44:26 -0500

    Replace requestPublisher with native AF combine code

--- Sources/Illithid/Extensions/Array.swift
+++ Sources/Illithid/Extensions/Array.swift
@@ -1,12 +1,12 @@
 //
 // Array.swift
 // Copyright (c) 2020 Flayware
-// Created by Tyler Gregory (@01100010011001010110010101110000) on 3/21/20
+// Created by Tyler Gregory (@01100010011001010110010101110000) on 7/13/20
 //
 
 import Foundation
 
-public extension Array {
+extension Array {
   var middle: Element? {
     guard !isEmpty else { return nil }
     return self[count / 2]
